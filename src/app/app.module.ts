3f9b7d4c734f507ffae1c98883a90ab7a62023c5
foridulislam21 <foridulislam21@gmail.com>
2021-01-21 00:24:56 +0600

    Design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './dashboard/about-me/about-me.component';
 import { HomeComponent } from './dashboard/home/home.component';
 import { NavbarComponent } from './dashboard/navbar/navbar.component';
 import { PicturePageComponent } from './dashboard/picture-page/picture-page.component';
-import { AboutMeComponent } from './dashboard/about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,8 @@ import { AboutMeComponent } from './dashboard/about-me/about-me.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBLUV4jbhi3GcVhN0Ptl-BPteBAC0k29mA',
+    authDomain: 'my-portfolio-page-89d9f.firebaseapp.com',
+    projectId: 'my-portfolio-page-89d9f',
+    storageBucket: 'my-portfolio-page-89d9f.appspot.com',
+    messagingSenderId: '186114294189',
+    appId: '1:186114294189:web:f523fa4ca259c1569da124',
+    measurementId: 'G-R6R9BPD6BT',
+  },
 };
