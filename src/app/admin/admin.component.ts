d102f145de07284d9891cfc491a1ef7df6423d0c
foridulislam21 <foridulislam21@gmail.com>
2021-02-19 22:54:00 +0600

    getting Ready for Linux

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -7,10 +7,22 @@ import { AuthService } from './auth/auth.service';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent implements OnInit {
-
-  constructor(public authService: AuthService) {}
+export class AdminComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  constructor(
+    public authService: AuthService,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
   ngOnInit() {}
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
   logout() {
     this.authService.signOut();
   }
